7ba1cc158d550f0bbd48698465638158d9d8f9ba
Brian Zoetewey <brian.zoetewey@ccci.org>
2012-08-06 18:34:52 +0000

    Issue #24 - WIP - Updated select walker to render a select element.

--- walkers/select.class.php
+++ walkers/select.class.php
@@ -3,17 +3,24 @@ class ACF_Taxonomy_Field_Select extends ACF_Taxonomy_Field_Walker {
 	
 	
 	public function start_lvl( &$output, $depth = 0, $args = array() ) {
-		$output .=
-			'<select ' .
-			'name="' . $this->field[ ACF_Taxonomy_Field::FIELD_NAME ] . '"';
-/*		<select name="<?php echo $field[ self::FIELD_NAME ]; ?>[]" id="<?php echo $field[ self::FIELD_NAME ]; ?>" class="<?php echo $field[ self::FIELD_CLASS ]; ?>" <?php echo ( $field[ self::FIELD_INPUT_TYPE ] == self::INPUT_TYPE_MULTISELECT ) ? 'multiple="multiple" size="' . $field[ self::FIELD_INPUT_SIZE ] . '"' : '';>'*/
+		$output .= sprintf(
+			'<select name="%1$s" id="%2$s" class="%3$s">',
+			esc_attr( $this->field[ ACF_Taxonomy_Field::FIELD_NAME ] . '[]' ),
+			esc_attr( $this->field[ ACF_Taxonomy_Field::FIELD_NAME ] ),
+			esc_attr( $this->field[ ACF_Taxonomy_Field::FIELD_CLASS ] )
+		);
 	}
 	
 	public function end_lvl( &$output, $depth = 0, $args = array() ) {
-		
+		$output .= '</select>';
 	}
 	
 	public function start_el( &$output, $object, $depth, $args, $current_object_id = 0 ) {
-		$output .= '<option value="' . esc_attr( $object->term_id ) . '" ' . selected( in_array( (int) $object->term_id, $this->field[ ACF_Taxonomy_Field::FIELD_VALUE ] ), true, false ) . '>' . str_repeat( '&nbsp;', $depth * 3 ) . esc_attr( $object->name ) . '</option>';
+		$output .= sprintf(
+			'<option value="%1$s" %2$s>%3$s</option>',
+			esc_attr( $object->term_id ),
+			selected( in_array( (int) $object->term_id, $this->field[ ACF_Taxonomy_Field::FIELD_VALUE ] ), true, false ),
+			str_repeat( '&nbsp;', $depth * 3 ) . esc_html( $object->name )
+		);
 	}
 }
